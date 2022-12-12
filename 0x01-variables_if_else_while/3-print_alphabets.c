45c5ac086d554bf25aacaf853859b9d5ff393be5
Francis Chibuike <francisjchibuike@gmail.com>
2022-12-11 18:02:04 -0800

    upperlower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';
+
+	while (ch < 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH < 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
