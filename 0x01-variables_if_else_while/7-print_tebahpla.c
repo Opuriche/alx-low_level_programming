002b5ed52d2b9394a7057e37443c22e71956ba80
Francis Chibuike <francisjchibuike@gmail.com>
2022-12-12 08:17:25 -0800

    printing alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - printing alphabets in reverse
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch = 'z';
+
+	while (ch > 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
