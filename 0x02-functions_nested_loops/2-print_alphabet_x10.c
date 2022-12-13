27685fe647bf75b3b29111bc24ae33c1765e7ef1
Francis Chibuike <francisjchibuike@gmail.com>
2022-12-13 15:38:54 -0800

    print alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Descriptrion - prints the alphabets in lower case 10 times
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void) /*function prints the alphabet in lower case*/
+{
+	char ch;
+	int i = 0;
+
+	while (i < 10)
+	{
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		i++;
+		print('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define MY_MAIN
 int _putchar(char);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif
