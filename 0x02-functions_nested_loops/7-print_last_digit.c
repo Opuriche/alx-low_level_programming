c8a800f5a765cba1173144c93d97a038aa9c222d
Francis Chibuike <francisjchibuike@gmail.com>
2022-12-13 18:06:35 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ *@n: integer in consideration
+ * Return: last digit value
+ */
+
+int print_last_digit(int n)
+{
+	int last_num = n % 10;
+
+	if (last_num < 0)
+		last_num *= -1;
+	_putchar(last_num + '0');
+	return (last_num);
+}
