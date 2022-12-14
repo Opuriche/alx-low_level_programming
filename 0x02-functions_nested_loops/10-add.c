cf541b959a0b851e282def241696ca5933cd836a
Francis Chibuike <francisjchibuike@gmail.com>
2022-12-14 13:41:58 -0800

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @n1: first integer
+ * @n2: second integer
+ * Return: 0
+ */
+
+int add(int n1, int n2)
+{
+	int result = n1 + n2;
+
+	_putchar(result);
+}
