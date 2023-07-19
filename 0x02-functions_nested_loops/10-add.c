da42a0698e5e560a93b3f1002966295d9430c605
Opuriche <chibuike>
2023-07-19 07:39:43 -0700

    0x02 mandatory tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+
+}
