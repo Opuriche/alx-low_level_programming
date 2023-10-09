0fe484cbda64959f7173ba38f2a3e696373f27fd
Opuriche <jstcmoses@gmail.com>
2023-10-09 03:22:18 -0700

    more malloc and free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,7 +9,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int is_digit(char *s);
 int _strlen(char *s);
-void error();
-int main(int argc, char *argv[]);
+void error(void);
 
 #endif
