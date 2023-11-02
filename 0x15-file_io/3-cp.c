8cdf5d61e6a9288b4b20ff83f859aa18c32cf4f9
Chibuike <jstcmoses@gmail.com>
2023-11-02 07:43:02 -0700

    cfile i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 		error_exit(98, "Error: Can't read from file %s\n", file_from);
 	}
 
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (fd_to == -1)
 	{
 		error_exit(99, "Error: Can't write to file %s\n", file_to);
